82b1406d2709b0cca7472aad69f7f8b44fb4029e
seosd <seosd97@gmail.com>
2020-07-05 17:11:33 +0900

    setup eslint

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 /* 
 1. 등록된 소환사 정보 (모스트 챔피언, 역대 DCK 등수 포함)
 */
+
+const restify = require('restify');
+
+const server = restify.createServer();
+const port = 8080;
+
+server.use(restify.plugins.bodyParser);
+
+server.get('/', (req, res) => {
+  res.send('DCK REGISTER');
+});
+
+server.listen(port, () => {
+  console.log(`DCK Register listening on ${port}`);
+});
